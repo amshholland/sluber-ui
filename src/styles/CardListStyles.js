f746184f2400f918868698a9d2aa2046f3619567
Anthony Joo <anthony.joo@slalom.com>
2020-10-01 17:03:22 -0700

    focus on only the wireframe

---
+++ src/styles/CardListStyles.js
@@ -0,0 +1,10 @@
+import { createStyles } from '@material-ui/core/styles';
+
+const CardListStyles = () =>
+    createStyles({
+        '.grid': {
+            passing: 24,
+    },
+});
+
+export default CardListStyles;
