d8fa33846630c750fb758f745ec9c17f8ee77250
Pragathi Shankar <pragathi.shankar@slalom.com>
2020-10-06 12:56:24 -0700

    added page template unit tests

--- src/components/TopMenu.test.js
+++ src/components/TopMenu.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render } from '@testing-library/react';
 import TopMenu from './TopMenu';
 
 test('renders departure search field', () => {
