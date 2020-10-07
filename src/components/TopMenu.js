9c054c13b2188f0a76a219bc183fc86a406f29d9
Pragathi Shankar <pragathi.shankar@slalom.com>
2020-10-07 10:14:13 -0700

    ui of post ride pop up work

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -5,14 +5,49 @@ import Button from '@material-ui/core/Button';
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from '@material-ui/core';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import HomeIcon from '@material-ui/icons/Home';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
+import EventSeatIcon from '@material-ui/icons/EventSeat';
+import FlightLandIcon from '@material-ui/icons/FlightLand';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
 
 class TopMenu extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.handlePostOpen = this.handlePostOpen.bind(this);
+    this.handlePostClose = this.handlePostClose.bind(this);
+    this.handleEmployeeChange = this.handleEmployeeChange.bind(this);
     this.state = {
-      departure: '',
-      destination: '',
+      isPostRideOpen: false,
+      employee: 'driver'
     }
+  }
+
+  handlePostOpen() {
+    this.setState({
+      isPostRideOpen: true
+    })
+  }
+
+  handlePostClose() {
+    this.setState({
+      isPostRideOpen: false
+    })
+  }
+
+
+  handleEmployeeChange(event) {
+    this.setState({
+      employee: event.target.value
+    })
   }
 
   render() {
@@ -54,9 +89,119 @@ class TopMenu extends Component {
           </form>
         </div>
       <div className='post-ride-btn-cont'>
-        <Button variant='contained' color='primary' defaultValue={this.state.date} onChange={this.handleDateChange}>
+        <Button variant='contained' color='primary' onClick={this.handlePostOpen}>
           Post a Ride
         </Button>
+        <Dialog open={this.state.isPostRideOpen} onClose={this.handlePostClose}>
+          <DialogContent>
+            <FormControl component="fieldset">
+  +           <FormLabel component="legend">Employee Type</FormLabel>
+  +             <RadioGroup row aria-label="employee-type" name="employee-type" value={this.state.employee} onChange={this.handleEmployeeChange}>
+  +               <FormControlLabel value="driver" control={<Radio color="primary"/>} label="I'm a driver" />
+  +            <FormControlLabel value="passenger" control={<Radio color="primary"/>} label="I'm a passenger" />
+  +          </RadioGroup>
+  +        </FormControl>
+            <TextField 
+              id="post-name" 
+              label="Name" 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <IconButton>
+                      <PermContactCalendarIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+            <TextField 
+              id="post-phone-number" 
+              label="Phone Number" 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <IconButton>
+                      <PhoneAndroidIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+            <TextField 
+              id="post-origin" 
+              label="Origin"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <IconButton>
+                      <HomeIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }} 
+            />
+            <TextField 
+              id="post-departure" 
+              label="Departure Time" 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <IconButton>
+                      <ScheduleIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }} 
+            />
+            <TextField 
+              id="post-destination" 
+              label="Destination" 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <IconButton>
+                      <LocationOnIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }} 
+            />
+            <TextField 
+              id="post-est-arrival" 
+              label="Estimated Arrival Time" 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <IconButton>
+                      <FlightLandIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }} 
+            />
+            <TextField 
+              id="post-seats" 
+              label="Seats Available" 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <IconButton>
+                      <EventSeatIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handlePostClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handlePostClose} color="primary">
+              Submit
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </div>
     );
