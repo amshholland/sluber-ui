00aa419c2470ad83f04c9ffe7dc130fc997737f5
Amber Holland <amber.holland@slalom.com>
2021-09-21 14:13:59 -0400

    created google auth/firebase files

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaQ0-JN1tYDLhtcMr8yiwlV1sJ1SZvp5c",
+  authDomain: "sluber-2d4d3.firebaseapp.com",
+  projectId: "sluber-2d4d3",
+  storageBucket: "sluber-2d4d3.appspot.com",
+  messagingSenderId: "554097181366",
+  appId: "1:554097181366:web:2da68e6f1ce93ea1fc5c17"
+};
+firebase.initializeApp(firebaseConfig);
+
+export const authProvider = new firebase.auth.GoogleAuthProvider();
+
+export function signInWithGoogle(): void {
+    firebase.auth().signInWithPopup(authProvider);
+}
+export function signOut(): void {
+    firebase.auth().signOut();
+}
+
+export default firebase;
