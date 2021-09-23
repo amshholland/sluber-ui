3f1ac22a7239621af7e7cef5d859e16a299c9511
Amber Holland <amber.holland@slalom.com>
2021-09-23 10:35:56 -0400

    Successfully showing splash page while FRP loads

---
+++ src/components/SplashPage.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export function SplashPage() {
+    return (
+        <div className='SplashPage'>
+            <p> Loading... </p>
+            <img src='%PUBLIC_URL%/favicon.ico"' />
+        </div>
+    )
+}
