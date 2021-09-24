624f3f33f44f00ff22d130f6c6d998997364104f
Amber Holland <amber.holland@slalom.com>
2021-09-24 09:17:53 -0400

    changed dependencies for testing purposes

---
+++ src/tests/TopMenu.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import TopMenu from "../components/TopMenu";
+
+test("renders departure search field", () => {
+	const topMenu = render(<TopMenu />);
+	const divByClass = document.querySelector(".departure-search-cont");
+	expect(divByClass.textContent).toEqual("Departure");
+});
+
+test("renders destination search field", () => {
+	const topMenu = render(<TopMenu />);
+	const divByClass = document.querySelector(".destination-search-cont");
+	expect(divByClass.textContent).toEqual("Destination");
+});
+
+test("renders date search field", () => {
+	const topMenu = render(<TopMenu />);
+	const divByClass = document.querySelector(".date-search-cont");
+	expect(divByClass.textContent).toEqual("Date");
+});
